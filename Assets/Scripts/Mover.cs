7362fac82c0dca2d752a4640f342a78d2c5b3e27
TestSubject71 <ethosislami07@gmail.com>
2026-09-13 17:20:19 +0700

    Fix - script

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,5 +1,3 @@
-using System.IO.Compression;
-using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
@@ -7,7 +5,7 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(1, 0, 0);
+        
         
     }
 
