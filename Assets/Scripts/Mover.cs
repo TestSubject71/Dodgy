e28248a8e161cbd584bda9c732b6c13417027484
TestSubject71 <ethosislami07@gmail.com>
2026-09-13 16:27:44 +0700

    ADD - Initialize

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks.Dataflow;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.Translate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
